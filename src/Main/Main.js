20ae343a0ef659004b5a51c38b71d96b78a9cb96
faridulhaque <faridulhaquemurshed@gmail.com>
2022-06-25 13:29:01 +0600

    styles updated

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import Banner from "../Home/Banner";
 import PetShop from "../Home/PetShop";
 import Services from "../Home/Services";
-import Navbar from "../Shared/Navbar/Navbar";
+
 
 const Main = () => {
   return (
